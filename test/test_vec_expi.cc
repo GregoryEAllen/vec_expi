f4e1a02a29269f47a8e76d64c882cd4e08ff0dd8
Greg Allen <GregoryEAllen@yahoo.com>
2017-06-20 22:08:33 -0500

    Add test_overrun to test_vec_expi

--- test/test_vec_expi.cc
+++ test/test_vec_expi.cc
@@ -30,6 +30,10 @@
 #include <iostream>
 #include <sys/time.h>
 
+#include <unistd.h>
+#include <sys/mman.h>
+#include <cstdio>
+
 double current_time(void)
 {
     struct timeval tv;
@@ -37,7 +41,7 @@ double current_time(void)
     return tv.tv_sec + tv.tv_usec/1e6;
 }
 
-int main(int argc, const char* argv[])
+int test_correctness(void)
 {
     const unsigned maxN = 256;
 
@@ -92,5 +96,92 @@ int main(int argc, const char* argv[])
             std::cout << "vec_expi " << size << " err: " << err << std::endl;
         }
     }
+    return err_count;
+}
+
+int test_overrun(void)
+{
+    unsigned err_count = 0;
+    unsigned pagesz = getpagesize();
+
+    // mmap 4 pages
+    char *buf = (char*)mmap(0, 4*pagesz, PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
+    if (buf == MAP_FAILED) {
+        perror("mmap error");
+        return ++err_count;
+    }
+    // munmap to make 2 holes
+    if (munmap(buf+pagesz, pagesz)) {
+        perror("munmap error");
+        return ++err_count;
+    }
+    if (munmap(buf+3*pagesz, pagesz)) {
+        perror("munmap error");
+        return ++err_count;
+    }
+
+    const unsigned maxN = 16;
+
+    Eigen::VectorXf in0(maxN);
+    Eigen::VectorXcf out0(maxN);
+
+    float *inp = (float*)(buf);
+    std::complex<float> *outp = (std::complex<float>*)(buf+2*pagesz);
+    Eigen::Map<Eigen::VectorXf> in1(inp, pagesz/sizeof(float));
+    Eigen::Map<Eigen::VectorXcf> out1(outp, pagesz/sizeof(float)/2);
+    //in1.tail(1).data()[1] = 0; // SEGFAULT
+    //out1.tail(1).data()[1] = 0; // SEGFAULT
+
+    for (unsigned size=1; size<=maxN; ++size) {
+        Eigen::Ref<Eigen::ArrayXf> in0r = in0.head(size);
+        Eigen::Ref<Eigen::ArrayXf> in1r = in1.tail(size);
+        Eigen::Ref<Eigen::ArrayXcf> out0r = out0.head(size);
+        Eigen::Ref<Eigen::ArrayXcf> out1r = out1.tail(size);
+        in0r = Eigen::ArrayXf::Random(size) * M_PI;
+        in1r = in0r;
+        vec_expi_libm(in0r.data(), out0r.data(), size);
+        vec_expi_libm(in1r.data(), out1r.data(), size);
+        if ((out0r-out1r).matrix().norm()) {
+            err_count++;
+            std::cout << "libm " << size << " err" << std::endl;
+        }
+#ifdef __SSE2__
+        vec_expi_sse2(in0r.data(), out0r.data(), size);
+        vec_expi_sse2(in1r.data(), out1r.data(), size);
+        if ((out0r-out1r).matrix().norm()) {
+            err_count++;
+            std::cout << "sse2 " << size << " err" << std::endl;
+        }
+#endif
+#ifdef __AVX__
+        vec_expi_avx(in0r.data(), out0r.data(), size);
+        vec_expi_avx(in1r.data(), out1r.data(), size);
+        if ((out0r-out1r).matrix().norm()) {
+            err_count++;
+            std::cout << "avx " << size << " err" << std::endl;
+        }
+#endif
+        vec_expi(in0r.data(), out0r.data(), size);
+        vec_expi(in1r.data(), out1r.data(), size);
+        if ((out0r-out1r).matrix().norm()) {
+            err_count++;
+            std::cout << "vec_expi " << size << " err" << std::endl;
+        }
+    }
+
+    // munmap all of it
+    if (munmap(buf, 4*pagesz)) {
+        perror("munmap error");
+        return ++err_count;
+    }
+
+    return err_count;
+}
+
+int main(int argc, const char* argv[])
+{
+    unsigned err_count = 0;
+    err_count += test_correctness();
+    err_count += test_overrun();
     return err_count;
 }
